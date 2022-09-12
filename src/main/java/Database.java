f6c5d5290f2a6295b2da496fa9ea164ebce5ef72
OmarKayed <omar2346@stud.kea.dk>
2022-09-12 19:20:06 +0200

    Added database + creation of heroes

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,3 +1,14 @@
 public class Database {
+private Superhero [] superheroes;
+private int superheroesInTotal;
+
+public Database(){
+    superheroes = new Superhero[5];
+    superheroesInTotal = 0;
+}
+public void createSuperhero (String navn, String kræfter, int fødtIÅrstal, String ægtenavn){
+    Superhero superhero = new Superhero(navn, kræfter,fødtIÅrstal, ægtenavn);
+    superheroes[superheroesInTotal++] = superhero;
+    }
 
 }
