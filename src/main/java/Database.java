b73b9e1920f6d32474832bd7e22d24b9291d4bd4
OmarKayed <omar2346@stud.kea.dk>
2022-09-13 09:23:19 +0200

    Skabt en boolean for at kunne indtage brugerens input om superhelten er et menneske eller ej

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,12 +2,16 @@ public class Database {
 private Superhero [] superheroes;
 private int superheroesInTotal;
 
+
+// Holder styr på hvor mange helte der er at arbejde med, som fx 5 superhelte
 public Database(){
     superheroes = new Superhero[5];
+    // Når du opretter en superhelt starter man på 0, hvilket er hvorfor at totalen er 0
     superheroesInTotal = 0;
 }
-public void createSuperhero (String navn, String kræfter, int fødtIÅrstal, String ægtenavn){
-    Superhero superhero = new Superhero(navn, kræfter,fødtIÅrstal, ægtenavn);
+// Fremfor at min main laver en superhero, så gør min database det inde i mit array
+public void createSuperhero (String navn, String kræfter, int fødtIÅrstal, String ægteNavn, boolean heroIsReal){
+    Superhero superhero = new Superhero(navn, kræfter,fødtIÅrstal, ægteNavn);
     superheroes[superheroesInTotal++] = superhero;
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    // Databasen bliver sat ind, eftersom at vi skal bruge dens data for at kunne skabe superhelte
+    public static Database database = new Database();
     public static void main(String[] args){
         System.out.println("Velkommen til superhelte programmet");
         System.out.println("1. Opret superhelt");
         System.out.println("9. Afslut");
+        // Programmet bliver sat ind i et loop, da vi har to valgmuligheder. enten at oprette en helt eller afslutte programmet
         int input = scanner.nextInt();
         if (input==1){
             System.out.println("Indtast din superhelts navn ");
             String heroName = scanner.next();
+
+            System.out.println("Indtast superheltens rigtige navn");
+            String heroReal = scanner.next();
 
             System.out.println("Hvilke kræfter har din superhelt?");
             String heroPower = scanner.next();
@@ -17,9 +23,19 @@ public class Main {
             System.out.println("Hvornår blev din superhelt skabt?");
             int heroCreation = scanner.nextInt();
 
-            System.out.println("Er din superhelt et ægte menneske? Y/N");
-            String heroReal = scanner.next();
-            Superhero superhero1 = new Superhero(heroName, heroPower, heroCreation, heroReal);
+// En boolean bliver skabt nu for at kunne identificere bruges input og ønske om at superhelten er ægte eller ej
+            System.out.println("Er din superhelt et ægte menneske? J/N");
+            char heroRealInput = scanner.next().charAt(0);
+            boolean heroIsReal = false;
+            if (heroRealInput == 'j'){
+                heroIsReal = true;
+            }else if (heroRealInput == 'n'){
+                heroIsReal = false;
+            }else{
+                System.out.println("Ugyldigt input");
+            }
+            // Databasens input kan nu blive brugt her
+            database.createSuperhero(heroName,heroPower,heroCreation,heroReal, heroIsReal);
 
         } else if(input==9){
             System.exit(1);
